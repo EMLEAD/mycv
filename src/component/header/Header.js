10b002145db10357554a7a225348f63145e64e9f
ADEWALE MOSES OLAOLUWA <adewalem88@gmail.com>
2025-08-30 21:54:07 +0100

    Added te Hamburger icon

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,30 +1,33 @@
-import React from 'react'
-import "./Header.css"
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react';
+import "./Header.css";
+import { NavLink } from 'react-router-dom';
+import Logo from "../assets/photo_2025-08-19_09-48-36.jpg";
+import { FaBars, FaTimes } from 'react-icons/fa';
 
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-const header = () => {
+  const handleToggle = () => setMenuOpen(!menuOpen);
+  const handleClose = () => setMenuOpen(false);
+
   return (
-    <div className="Main-Div">
-      <header>
-       
-        <nav>
-          <ul>
-          <li> <NavLink to="/">HOME</NavLink></li>
-          <li> <NavLink to="/About">ABOUT</NavLink></li>
-          <li> <NavLink to="/Services">SERVICES</NavLink></li>
-          <li> <NavLink to="/Contact">CONTACT</NavLink></li>
+    <header>
+      <div className="logo-div">
+        <img className="logo" src={Logo} alt="Logo" />
+      </div>
+      <nav>
+        <div className="hamburger" onClick={handleToggle}>
+          {menuOpen ? <FaTimes size={28} /> : <FaBars size={28} />}
+        </div>
+        <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
+          <li><NavLink to="/" onClick={handleClose}>HOME</NavLink></li>
+          <li><NavLink to="/About" onClick={handleClose}>ABOUT</NavLink></li>
+          <li><NavLink to="/Services" onClick={handleClose}>SERVICES</NavLink></li>
+          <li><NavLink to="/Contact" onClick={handleClose}>CONTACT</NavLink></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-          </ul>
-        </nav>
-      </header>
-      
-    
-  
-
-
-    </div>
-  )
-}
-
-export default header
+export default Header;
